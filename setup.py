a93ed3be0ad7b59568888d7f857c02445d57dd96
Joseph Lynch <joe.e.lynch@gmail.com>
2017-05-14 14:35:30 -0700

    Setup basic linting

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='python_performance_toolkit',
+    description=(
+        'Some one off performance scripts. Note this is not a python package, '
+        'but rather a collection of useful scripts and notebooks. Each script '
+        'has it\'s own dependencies (which are typically minimal)'
+    ),
+    url='https://github.com/jolynch/python_performance_toolkit',
+    version='1.0.0',
+    author='Joseph Lynch',
+    author_email='joe.e.lynch@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=[],
+    install_requires=[],
+)
